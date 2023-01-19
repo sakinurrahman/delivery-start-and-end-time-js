1116e0d08d410387e860bf10f438350d186a7d81
Sakinur Rahman <61472630+sakinurrahman@users.noreply.github.com>
2023-01-19 17:39:50 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
- function deliveryBetweenTimes(start,end){
-  let today = new Date(); 
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  let deliveryStart = new Date(today.getFullYear(),today.getMonth(),today.getDate()+start);
-  let deliveryEnd = new Date(today.getFullYear(),today.getMonth(),today.getDate()+end);
-  let startDate = deliveryStart.getDate();
-  let endDate = deliveryEnd.getDate();
+    function deliveryBetweenDate(){
+      // Get month Names 
+      const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      // Assigning Delivery initial and End Date.
+      let date = new Date();
+      let initialDeliveryDate = new Date(date) ;
+       console.log(initialDeliveryDate)
+      initialDeliveryDate.setDate(initialDeliveryDate.getDate() + 3);
+ console.log(initialDeliveryDate)
+      let endDeliveryDate = new Date(date) ;
+      endDeliveryDate.setDate(endDeliveryDate.getDate() + 5);
+  
+      // Chekcing Weekends for initail date
+      if ( initialDeliveryDate.getDay() === 0 || initialDeliveryDate.getDay() === 6) {
+        if (initialDeliveryDate.getDay() === 6) {
+          initialDeliveryDate.setDate(initialDeliveryDate.getDate() + 2)
+          endDeliveryDate.setDate(endDeliveryDate.getDate() + 2);
+        } else {
+          initialDeliveryDate.setDate(initialDeliveryDate.getDate() + 2)
+          endDeliveryDate.setDate(endDeliveryDate.getDate() + 2);
+        }
+      }
+     
 
-  // Checking if less than 10
-  startDate = startDate < 10 ?  '0' + startDate : startDate; 
-  endDate = endDate < 10 ? '0' + endDate : endDate; 
+       // Chekcing Weekends for ending date
+       if ( endDeliveryDate.getDay() === 0 || endDeliveryDate.getDay() === 6) {
+        if (endDeliveryDate.getDay() === 6) {
+          endDeliveryDate.setDate(endDeliveryDate.getDate() + 2)
+        } else {
+          endDeliveryDate.setDate(endDeliveryDate.getDate() + 2)
+        }
+      }
 
-  //Outpuut
-  let html = `
-  Lieferung zwischen 
-  <b>${startDate}.
-   ${months[deliveryStart.getMonth()]}
-   </b> und <b>${endDate}.
-   ${months[deliveryEnd.getMonth()]}
-   </b>` 
-   document.querySelector('.h1').innerHTML = html;
- }
- document.addEventListener('DOMContentLoaded', deliveryBetweenTimes(3,5))
+      // Getting Delivery Start Date and Month Name
+      let FinaldeliverySratDate = initialDeliveryDate.getDate();
+      let FinaldeliverySratMonthName = initialDeliveryDate.getMonth();
+
+      // Getting Delivery Start Date and Month Name
+      let finalDeliveryEndDate = endDeliveryDate.getDate();
+      let finalDeliveryEndMonth = endDeliveryDate.getMonth();
+
+
+
+       let html = `
+        Lieferung zwischen 
+        <b>${FinaldeliverySratDate} ${months[FinaldeliverySratMonthName]}.
+         </b> und <b>${finalDeliveryEndDate} ${months[finalDeliveryEndMonth]}.
+         </b>` 
+         document.querySelector('#delivery_message').innerHTML = html;
+    }
